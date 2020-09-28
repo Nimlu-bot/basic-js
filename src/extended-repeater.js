dbc0187dda28001ebd578985558045b0064a6727
Nimlu <snesterov@gmail.com>
2020-09-28 22:17:42 +0300

    feat: solve transform array

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,23 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-	let resultString = str;
+
 	let { repeatTimes, separator = '+', addition, additionRepeatTimes, additionSeparator = '|' } = options;
-	if (Number.isInteger(repeatTimes)) {
-		for (let i = 1; i <= repeatTimes; i++) {
-			resultString = resultString + str.toString();
-			if (Number.isInteger(additionRepeatTimes)) {
-				for (let j = 1; j <= repeatTimes; j++) {
-					resultString = resultString + addition.toString();
-					if (j < additionRepeatTimes) { resultString = resultString + additionSeparator.toString() }
+	// if (Number.isInteger(repeatTimes)) {
+	// 	for (let i = 1; i <= repeatTimes; i++) {
+	// 		resultString = resultString + str.toString();
+	// 		if (Number.isInteger(additionRepeatTimes)) {
+	// 			for (let j = 1; j <= repeatTimes; j++) {
+	// 				resultString = resultString + addition.toString();
+	// 				if (j < additionRepeatTimes) { resultString = resultString + additionSeparator.toString() }
 
-				}
-			}
-			if (i < repeatTimes) { resultString = resultString + separator.toString() }
-		}
-		return resultString;
-	}
+	// 			}
+	// 		}
+	// 		if (i < repeatTimes) { resultString = resultString + separator.toString() }
+	// 	}
+	// 	return resultString;
+	// }
 
+	let addStr = new Array(additionRepeatTimes).fill(String(addition)).join(additionSeparator);
+
+	return new Array(repeatTimes).fill(str + addStr).join(separator);
+};
 	//throw new CustomError('Not implemented');
 	// remove line with error and write your code here
-};
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,103 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/*arr*/) {
-	/*	if (arr instanceof Array) {
-	
-			const transformArray = arr.slice();
-			/*
-					for (let i = 0; i <= transformArray.length; i++) {
-						switch (transformArray[i]) {
-							case '--discard-next': { transformArray.splice(i, 2, undefined, undefined) };
-								break;
-							case "--discard-prev": { transformArray.splice(i - 1, 2, undefined, undefined) };
-								break;
-							case "--double-next": { transformArray.splice(i, 1, undefined, transformArray[i + 1]) };
-								break;
-							case "--double-prev": { transformArray.splice(i - 1, 1, transformArray[i - 1], undefined) };
-								break;
-			
-						}
-			
-					}
-					let trArr = transformArray.filter(item => item !== undifined)
-					return trArr;
-			*/
 
+module.exports = function transform(arr) {
+	if (Array.isArray(arr)) {
+		let result = [];
 
-
-	/*let i = 1;
-	if (transformArray[0] == "--discard-prev" || transformArray[0] == "--double-prev") {
-		transformArray[0] = undefined;
-	}
-	else if (transformArray[0] == "--double-next") {
-		transformArray[0] = transformArray[1];
-
-	}
-	else if (transformArray[0] == "--discard-next") {
-		transformArray[0] = undefined;
-		transformArray[1] = undefined;
-	}
-
-	else if (i < transformArray.length) {
-		for (i = 1; i <= transformArray.length - 1; i++) {
-			switch (transformArray[i]) {
-				case '--discard-next': { transformArray.splice(i, 2, undefined, undefined) };
+		for (let i = 0; i < arr.length; i++) {
+			switch (arr[i]) {
+				case '--discard-prev':
+					if (result.length !== 0 && arr[i - 2] !== '--discard-next') result.pop();
 					break;
-				case "--discard-prev": { transformArray.splice(i - 1, 2, undefined, undefined) };
+
+				case '--double-prev':
+					if (i !== 0 && arr[i - 2] !== '--discard-next') result.push(arr[i - 1]);
 					break;
-				case "--double-next": { transformArray.splice(i, 2, transformArray[i + 1], transformArray[i + 1]) };
+
+				case '--double-next':
+					if (i < arr.length - 1) result.push(arr[i + 1]);
 					break;
-				case "--double-prev": { transformArray.splice(i - 1, 2, transformArray[i - 1], transformArray[i - 1]) };
+
+				case '--discard-next':
+					i++;
 					break;
-			}
-		}
+
+				default:
+					result.push(arr[i]);
+			};
+		};
+
+		return result;
+	} else {
+		throw new Error();
 	}
-	else if (transformArray[transformArray.length] == "--discard-next" || transformArray[transformArray.length] == "--double-next") {
-		transformArray[transformArray.length] = undefined;
-
-	}
-	else if (transformArray[transformArray.length] == "--double-prev" && transformArray.length > 1) {
-		transformArray[transformArray.length] = transformArray[transformArray.length - 1];
-	}
-	else if (transformArray[transformArray.length] == "--discard-prev" && transformArray.length > 1) {
-		transformArray[transformArray.length] = undefined;
-		transformArray[transformArray.length - 1] = undefined;
-	}
-	let trArr = transformArray.filter(item => item !== undefined);
-	return trArr;
-}
-
-else throw ("Error");
-
-
-*/
-
-	/* if (!Array.isArray(arr)) throw Error
-	let subArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === '--discard-next') {
-
-			i += 1;
-		} else if (arr[i] === '--discard-prev') {
-			subArr.pop();
-		} else if (arr[i] === '--double-next' && i < arr.length - 1) {
-			subArr.push(arr[i + 1]);
-		} else if (arr[i] === '--double-prev' && i > 0) {
-			subArr.push(arr[i - 1]);
-		}
-		else if (arr[i] === '--double-next' && i == arr.length - 1) {
-
-		}
-		else if (arr[i] === '--double-prev' && (i == 0 || arr[i - 2] === '--discard-next')) {
-
-		}
-		else {
-			subArr.push(arr[i]);
-		}
-	}
-	return subArr
-	*/
-	throw new CustomError('Not implemented');
-	// remove line with error and write your code here
-
 };
