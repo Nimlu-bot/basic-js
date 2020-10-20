1e4edaa9f036ed3dd74965489d00360f75fd34cf
Nimlu <snesterov@gmail.com>
2020-10-20 22:56:13 +0300

    feat: solve all tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+
+	constructor(Direct) {
+		this.Direct = (Direct === false);
+	}
+
+
+	encrypt(string, key) {
+		if (!string || !key) throw new Error;
+		key = key.repeat(Math.ceil(string.length / key.length));
+		let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = "";
+		let keyIndex = 0;
+		for (let i = 0; i < string.length; i++) {
+			if (/^[a-zA-Z]{1}$/.test(string[i])) {
+				result += alphabet.charAt((alphabet.indexOf(string.charAt(i)) + alphabet.indexOf(key.charAt(i - keyIndex))) % alphabet.length);
+			}
+			else {
+				result += string[i];
+				keyIndex++;
+			};
+
+		}
+		result = this.Direct ? [...result].reverse().join('') : result;
+		return result.toUpperCase();
+		//throw new CustomError('Not implemented');
+		// remove line with error and write your code here
+	}
+	decrypt(string, key) {
+		if (!string || !key) throw new Error;
+		key = key.repeat(Math.ceil(string.length / key.length));
+		let keyIndex = 0;
+		let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = "";
+		for (let i = 0; i < string.length; i++) {
+			if (/^[a-zA-Z]{1}$/.test(string[i])) {
+				result += alphabet.charAt(((alphabet.indexOf(string.charAt(i)) + alphabet.length) - alphabet.indexOf(key.charAt(i - keyIndex))) % alphabet.length);
+			}
+			else {
+				result += string[i];
+				keyIndex++;
+			};
+		}
+		result = this.Direct ? [...result].reverse().join('') : result;
+		return result.toUpperCase();
+
+	}
 }
 
 module.exports = VigenereCipheringMachine;
